7ce43e72507611d7b88bf89408aeb722fdcdf878
Shadowpatriot9 <gscovil9@gmail.com>
2025-02-01 12:22:12 -0700

    Initial Commit

---
+++ STE/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STE
+//
+//  Created by Grayden Scovil on 2/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
